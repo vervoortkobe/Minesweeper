d35951299e68c9b0c59dde74acae71cc26cb0509
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-11-06 00:37:22 +0100

    add click&reveal

--- game.c
+++
@@ -1,7 +0,0 @@
-//
-// Created by vervo on 19/10/2025.
-//
-/*
- * game logic
- * open up empty connected boxes, after first click on an empty box
- */
--- screenhandler.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by vervo on 14/10/2025.
-//
-
